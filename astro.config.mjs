bad51bbf315fdd32cbaeae29eb166416c3c99e9d
241443Mooks <132487762+241443Mooks@users.noreply.github.com>
2025-08-19 21:08:28 +0000

    Add @ alias to Astro config for imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   site: 'https://smooks.co.uk',
   integrations: [mdx(), tailwind(), sitemap()],
+  aliases: {
+    '@': fileURLToPath(new URL('./src', import.meta.url)),
+  },
 });
