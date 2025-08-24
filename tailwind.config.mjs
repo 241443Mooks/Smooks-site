fbdf3f9a02ca1d15365c50ea4aa9ae36d63a2f6a
241443Mooks <132487762+241443Mooks@users.noreply.github.com>
2025-08-24 19:54:29 +0100

    chore: remove unused typography plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,8 @@
-import typography from "@tailwindcss/typography";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx,vue,svelte}"
   ],
   theme: { extend: {} },
-  plugins: [typography],
+  plugins: [],
 };
